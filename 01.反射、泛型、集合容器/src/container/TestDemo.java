e267c02c5d0cda25912329cc8633d9dd2c6488c0
zsp <15396226187@163.com>
2021-06-17 01:22:35 +0800

    查看并学习了ArrayList源码

---
+++ 01.反射、泛型、集合容器/src/container/TestDemo.java
@@ -0,0 +1,10 @@
+package container;
+
+import java.util.ArrayList;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+        list.add("!");
+    }
+}
