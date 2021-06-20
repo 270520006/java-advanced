f523430cced34beb373c211c626a993bc605299b
zsp <15396226187@163.com>
2021-06-20 18:54:31 +0800

    进入函数式编程深入学习

--- 01.反射、泛型、集合容器/src/container/TestLinkList.java
+++ 01.反射、泛型、集合容器/src/container/TestLinkList.java
@@ -6,5 +6,8 @@ public class TestLinkList {
     public static void main(String[] args) {
         LinkedList<Object> linkedList = new LinkedList<>();
         linkedList.add("asd");
+        linkedList.get(0);
+        Object remove = linkedList.remove(0);
+        System.out.println(remove);
     }
 }
